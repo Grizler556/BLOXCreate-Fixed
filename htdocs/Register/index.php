2ff8dc3e5885969eafcc2f14aee9ed838136484b
Grizler556 <102486638+Grizler556@users.noreply.github.com>
2022-09-03 07:17:41 +0200

    Update index.php

--- htdocs/Register/index.php
+++ htdocs/Register/index.php
@@ -4,13 +4,15 @@
 <div class="content-box" style="min-width:400px;width:400px;margin:0 auto;">
 <h5 style="padding:0;margin:0;padding-bottom:25px;">Register</h5>
 <form action="" method="POST">
-<input type="text" name="email" id="email" class="general-textarea" placeholder="Email Address" name="csrf_token" value="VoCrOP9owOTkoCXLNxpR5AkeGaiTYAhk1xH1Bjyivso=">
+<input type="text" name="email" id="email" class="general-textarea" placeholder="Email Address">
 <div style="height:15px;"></div>
 <input type="text" name="username" id="username" class="general-textarea" placeholder="Username">
 <div style="height:15px;"></div>
-<input type="password" name="password" id="password" class="general-textarea" placeholder="Password" name="csrf_token" value="VoCrOP9owOTkoCXLNxpR5AkeGaiTYAhk1xH1Bjyivso=">
+<input type="password" name="password" id="password" class="general-textarea" placeholder="Password">
 <div style="height:15px;"></div>
-<input type="password" name="confirmpassword" id="confirmpassword" class="general-textarea" placeholder="Password (again)" name="csrf_token" value="VoCrOP9owOTkoCXLNxpR5AkeGaiTYAhk1xH1Bjyivso=">
+<input type="password" name="confirmpassword" id="confirmpassword" class="general-textarea" placeholder="Password (again)">
 <div style="height:15px;"></div>
-<input type="submit" name="submit" class="groups-blue-button" style="padding:0;padding:4px 8px;" value="Register" name="csrf_token" value="VoCrOP9owOTkoCXLNxpR5AkeGaiTYAhk1xH1Bjyivso=">
+<div class="g-recaptcha brochure__form__captcha" data-sitekey="RECAPTCHA KEY"></div>
+<input type="hidden" name="csrf-token" value="CIwNZNlR4XbisJF39I8yWnWX9wX4WFoz"/>
+<input type="submit" name="submit" class="groups-blue-button" style="padding:0;padding:4px 8px;" value="Register">
 </form> </div> <?php include('../footer.php'); ?>
